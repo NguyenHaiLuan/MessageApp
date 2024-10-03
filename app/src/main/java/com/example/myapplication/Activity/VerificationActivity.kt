eaa75eec11c27011f68499c790a61c1ce3189039
NguyenHaiLuan <nguyenhailuan609@gmail.com>
2024-10-03 17:09:14 +0700

    hoan thanh code giao xac thuc OTP

--- app/src/main/java/com/example/myapplication/Activity/VerificationActivity.kt
+++ app/src/main/java/com/example/myapplication/Activity/VerificationActivity.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication.Activity
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,9 +14,19 @@ import androidx.core.widget.addTextChangedListener
 import com.airbnb.lottie.LottieDrawable
 import com.example.myapplication.R
 import com.example.myapplication.databinding.ActivityVerificationBinding
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 
 class VerificationActivity : AppCompatActivity() {
     private lateinit var bind: ActivityVerificationBinding
+    var auth: FirebaseAuth? = null
+    var verificationId: String? = null
+    var dialog: ProgressDialog? = null
+    var otp: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +34,90 @@ class VerificationActivity : AppCompatActivity() {
         initAnimation()
         inputOTP()
 
+        dialog = ProgressDialog(this@VerificationActivity)
+        dialog!!.setMessage("Đang xác thực...")
+        dialog!!.setCancelable(false)
+        dialog!!.show()
+
+        auth = FirebaseAuth.getInstance()
+
+        var phoneNumber = intent.getStringExtra("phone_number")
+        bind.tvSdtVerify.text = phoneNumber
+
+        if (!phoneNumber.isNullOrEmpty()) {
+
+            // Xác thực số điện thoại tại đây
+            val options = PhoneAuthOptions.newBuilder(auth!!)
+                .setPhoneNumber(phoneNumber)
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this@VerificationActivity)
+                .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    override fun onVerificationCompleted(p0: PhoneAuthCredential) {
+                        Toast.makeText(this@VerificationActivity, "Thành công!", Toast.LENGTH_SHORT).show()
+
+                    }
+
+                    override fun onVerificationFailed(p0: FirebaseException) {
+                        Toast.makeText(this@VerificationActivity, "Xác thực số điện thoại thất bại", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+
+                    override fun onCodeSent(
+                        verifyId: String,
+                        forceResendingToken: PhoneAuthProvider.ForceResendingToken
+                    ) {
+                        super.onCodeSent(verifyId, forceResendingToken)
+                        dialog!!.dismiss()
+                        verificationId = verifyId
+                        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+                        bind.otp1.requestFocus()
+                    }
+                }).build()
+
+            PhoneAuthProvider.verifyPhoneNumber(options)
+        } else {
+            Toast.makeText(this@VerificationActivity, "Chưa nhận được số điện thoại", Toast.LENGTH_SHORT).show()
+        }
+
+        bind.otp6.addTextChangedListener(
+            afterTextChanged = {
+                if (bind.otp6.length() == 1) {
+                    getOTP()
+                    sendOTP()
+                }
+
+                if (bind.otp6.length() == 0) {
+                    bind.otp5.requestFocus()
+                }
+            })
+    }
+
+    private fun sendOTP() {
+        val credential = PhoneAuthProvider.getCredential(verificationId!!, otp!!)
+        auth!!.signInWithCredential(credential)
+            .addOnCompleteListener {task ->
+                if (task.isSuccessful){
+                    val intent = Intent(this@VerificationActivity, SetUpProfileActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    Toast.makeText(this@VerificationActivity, "Sai mã OTP", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun getOTP() {
+        var l1 = bind.otp1.text.toString()
+        var l2 = bind.otp2.text.toString()
+        var l3 = bind.otp3.text.toString()
+        var l4 = bind.otp4.text.toString()
+        var l5 = bind.otp5.text.toString()
+        var l6 = bind.otp6.text.toString()
+
+        otp = l1 + l2 + l3 + l4 + l5 + l6
+
+        Log.d("maOTPcheck", verificationId!!)
     }
 
     private fun initAnimation() {
@@ -32,8 +131,7 @@ class VerificationActivity : AppCompatActivity() {
 
         bind.otp1.addTextChangedListener(
             afterTextChanged = {
-                if (bind.otp1.length() == 1)
-                {
+                if (bind.otp1.length() == 1) {
                     bind.otp2.requestFocus()
                 }
 
@@ -41,22 +139,22 @@ class VerificationActivity : AppCompatActivity() {
 
         bind.otp2.addTextChangedListener(
             afterTextChanged = {
-                if (bind.otp2.length() == 1){
+                if (bind.otp2.length() == 1) {
                     bind.otp3.requestFocus()
                 }
 
-                if (bind.otp2.length() == 0){
+                if (bind.otp2.length() == 0) {
                     bind.otp1.requestFocus()
                 }
             })
 
         bind.otp3.addTextChangedListener(
             afterTextChanged = {
-                if (bind.otp3.length() == 1){
+                if (bind.otp3.length() == 1) {
                     bind.otp4.requestFocus()
                 }
 
-                if (bind.otp3.length() == 0){
+                if (bind.otp3.length() == 0) {
                     bind.otp2.requestFocus()
                 }
             })
@@ -64,41 +162,30 @@ class VerificationActivity : AppCompatActivity() {
 
         bind.otp4.addTextChangedListener(
             afterTextChanged = {
-                if (bind.otp4.length() == 1){
+                if (bind.otp4.length() == 1) {
                     bind.otp5.requestFocus()
                 }
 
-                if (bind.otp4.length() == 0){
+                if (bind.otp4.length() == 0) {
                     bind.otp3.requestFocus()
                 }
             })
 
         bind.otp5.addTextChangedListener(
             afterTextChanged = {
-                if (bind.otp5.length() == 1){
+                if (bind.otp5.length() == 1) {
                     bind.otp6.requestFocus()
                 }
 
-                if (bind.otp5.length() == 0){
+                if (bind.otp5.length() == 0) {
                     bind.otp4.requestFocus()
                 }
             })
 
-        bind.otp6.addTextChangedListener(
-            afterTextChanged = {
-                if (bind.otp6.length() == 1){
-                    sendOTPClick()
-                }
-
-                if (bind.otp6.length() == 0){
-                    bind.otp5.requestFocus()
-                }
-            })
-
-    }
-
-    private fun sendOTPClick(){
-
+        bind.btnVerify.setOnClickListener {
+            getOTP()
+            sendOTP()
+        }
     }
 
 
