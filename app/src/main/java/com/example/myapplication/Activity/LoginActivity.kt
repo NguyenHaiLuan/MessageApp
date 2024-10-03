ac4400daf6d3780b21dd1c35327d34903c93df7f
NguyenHaiLuan <nguyenhailuan609@gmail.com>
2024-10-03 17:08:47 +0700

    chinh sua Login va them setUpProfile Activity

--- app/src/main/java/com/example/myapplication/Activity/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/Activity/LoginActivity.kt
@@ -1,21 +1,41 @@
 package com.example.myapplication.Activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var bind: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initUI()
+        logInButtonClick()
+    }
 
+    private fun logInButtonClick(){
+        bind.btnLogIn.setOnClickListener {
+            val phoneNumber = bind.edtSdtLogIn.text.toString().trim()
+            if (phoneNumber.isNotEmpty()) {
+                val intent = Intent(this@LoginActivity, VerificationActivity::class.java)
+                intent.putExtra("phone_number", phoneNumber)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Vui lòng nhập số điện thoại", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
     private fun initUI(){
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        bind = ActivityLoginBinding.inflate(layoutInflater)
+        val view = bind.root
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/myapplication/Activity/SetUpProfileActivity.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication.Activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R
+
+class SetUpProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI(){
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_set_up_profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
